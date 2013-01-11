06e192b70b38580fb405368183bbed79923084f1
Pankaj Doharey <pankajdoharey@gmail.com>
2013-01-11 15:13:43 +0530

    Removed ugly if else branching to faster switch/case for tokenization scanner

--- src/lexer.rb
+++ src/lexer.rb
@@ -27,61 +27,59 @@ class Lexer
 
     current_char = code[i]
 
-    puts "Input code : #{CodeRay.scan(code, :java).encode :terminal}"
     while i < code.size
       chunk = code[i..-1]
-      paint("Character No : #{i}", :cyan)
-      paint("- Reduce : #{chunk}", :blue)
+      
       case chunk
       when /(\A[[:alnum:]]\w*)/ then 
         RESERVED_WORDS.include?($&) ? parsed_tokens << [$&.upcase.intern , $&] : parsed_tokens << [:IDENTIFIER, $&]
-        paint "Identifier : #{$&}", :red
+
       when /\A\(/       then parsed_tokens << [:BRACKET_OPEN,       $&]
-        paint "Identifier : #{$&}", :red
+
       when /\A\)/       then parsed_tokens << [:BRACKET_CLOSE,      $&]
-        paint "Identifier : #{$&}", :red
+
       when /\A([0-9])+/ then parsed_tokens << [:NUMBER ,            $&]
-        paint "Identifier : #{$&}", :red
+
       when /\A\".+\"/   then parsed_tokens << [:STRING ,            $&]
-        paint "Identifier : #{$&}", :red
+
       when /\A(\n)+/m   then parsed_tokens << [:NEWLINE,            $&]
-        paint "Identifier : #{$&}", :red
+
       when /\A(\+)/     then parsed_tokens << [:PLUS,               $&]
-        paint "Identifier : #{$&}", :red
+
       when /\A(\-)/     then parsed_tokens << [:MINUS,              $&]
-        paint "Identifier : #{$&}", :red
+
       when /\A(\*)/     then parsed_tokens << [:ASTERISK,           $&]
-        paint "Identifier : #{$&}", :red
+
       when /\A(\/)/     then parsed_tokens << [:SLASH,              $&]
-        paint "Identifier : #{$&}", :red
+
       when /\A(\&&)/    then parsed_tokens << [:AND,                $&]
-        paint "Identifier : #{$&}", :red
+
       when /\A(\|\|)/   then parsed_tokens << [:OR,                 $&]
-        paint "Identifier : #{$&}", :red
+
       when /\A(\==)/    then parsed_tokens << [:COMPARISON,         $&]
-        paint "Identifier : #{$&}", :red
+
       when /\A\!=/      then parsed_tokens << [:NOT_EQUALS,         $&]
-        paint "Identifier : #{$&}", :red
+
       when /\A(\!)/     then parsed_tokens << [:NOT,                $&]
-        paint "Identifier : #{$&}", :red
+
       when /\A\<=/      then parsed_tokens << [:LESSTHAN_EQUALS,    $&]
-        paint "Identifier : #{$&}", :red
+
       when /\A\>=/      then parsed_tokens << [:GREATERTHAN_EQUALS, $&]
-        paint "Identifier : #{$&}", :red
+
       when /\A(\<)/     then parsed_tokens << [:LESSTHAN,           $&]
-        paint "Identifier : #{$&}", :red
+
       when /\A(\>)/     then parsed_tokens << [:GREATERTHAN,        $&]
-        paint "Identifier : #{$&}", :red
+
       when /\A(\;)/     then parsed_tokens << [:SEMICOLON,          $&]
-        paint "Identifier : #{$&}", :red
+
       when /\A(\,)/     then parsed_tokens << [:COMMA,              $&]
-        paint "Identifier : #{$&}", :red
+
       when /\A(\:=)/    then parsed_tokens << [:SETSLOT,            $&]
-        paint "Identifier : #{$&}", :red
+
       when /\A(\=)/     then parsed_tokens << [:UPDATESLOT,         $&]
-        paint "Identifier : #{$&}", :red
+
       when /\A([[:blank:]])+/m    then identifier = nil
-        paint "Identifier : WHITESPACE", :white
+
       end
 
       unless $&.nil?
@@ -94,8 +92,6 @@ class Lexer
     parsed_tokens << [false, false]
     
     #Tokenized Output
-    puts "Parsed Tokens :\n #{CodeRay.scan(parsed_tokens.pretty_inspect, :ruby).encode :terminal}"
-    puts "Parsed Token Length  #{parsed_tokens.length}"
     return parsed_tokens
   end
 end
