b1b88dcbf8b91046b0283bc7075f7c4f2b537aa0
Pankaj Doharey <pankajdoharey@live.com>
2013-01-15 17:14:24 +0530

    Removed irrelevant code

--- src/lexer.rb
+++ src/lexer.rb
@@ -13,15 +13,6 @@ class Lexer
     #List All Parsed Tokens
     parsed_tokens = []
 
-    #Initialize Start Bracket
-    start_bracket = 0
-
-    #Initialize End Bracket
-    end_bracket = 0
-
-    #How Deep Are we nested
-    nesting_level = []
-
     #Initialize loop variable i to zero
     i = 0
 
