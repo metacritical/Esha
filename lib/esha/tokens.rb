67a9c9f5ce1d0aab55a163f83dca69c0f2c23259
Pankaj Doharey <pankajdoharey@gmail.com>
2013-01-16 18:26:09 +0530

    Somefiles renamed and deleted

--- lib/esha/tokens.rb
+++
@@ -1,8 +0,0 @@
-module Io
-  RESERVED_WORDS = 
-    [
-     'Object','Lobby','Protos', 'Number', 'File','String', 'Vector', 'Sequence', 'List', 'Date', 'Socket', 
-     'URL', 'Directory', 'Scheduler','System' , 'Networking', 'XML' , 'Future', 'Call', 'Coroutine', '(',
-     ')', ':=', ',', 'clone', 'method', '+', '-', '*', '/','list','Exception','println'
-    ]
-end
