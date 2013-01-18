676ba7c573534c3849003f00dee678a23c349df9
Pankaj Doharey <pankajdoharey@gmail.com>
2013-01-18 19:36:07 +0530

    Added Token class in tokens.rb - Now the lexer produces token objects instead of array of tokens

--- lib/io/tokens.rb
+++ lib/io/tokens.rb
@@ -6,4 +6,14 @@ module Io
      ')', ':=', ',', 'clone', 'method', '+', '-', '*', '/','list','Exception','println', 'forward', 'exit',
      'do'
     ]
+
+  class Token
+    attr_accessor :type , :value , :line , :column
+    def initialize(token)
+      self.type   = token[0]
+      self.value  = token[1]
+      self.column = token[2]
+      self.line   = token[3]
+    end
+  end
 end
