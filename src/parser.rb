d8da439a2b3a2e058fe5b24e5249bcc22aede694
Pankaj Doharey <pankajdoharey@gmail.com>
2013-01-19 17:27:43 +0530

    Added Token Read and look ahead methods in parser

--- src/parser.rb
+++ src/parser.rb
@@ -1,3 +1,28 @@
 class Parser
+  attr_reader :expressions #Expression stack
+  attr_accessor :lexer , :token , :read_token , :look_ahead
+
+  def initialize(lexer)
+    self.lexer = lexer
+    self.expressions = []
+  end
+
+  def parse
+
+  end
+
+  private
+  
+  def read_token
+    self.read_token = lexer.read_token
+    self.token = read_token
+  end
+
+  def look_ahead
+    lexer.look_ahead
+  end
+
+  def statement
     
+  end
 end
