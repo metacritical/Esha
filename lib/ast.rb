2c74fc45effab439b04d5d55936f0163bf72a8d5
Pankaj Doharey <pankajdoharey@gmail.com>
2013-01-19 17:27:01 +0530

    Added AST module for datatypes and structures also created AST::Number class

---
+++ lib/ast.rb
@@ -0,0 +1,9 @@
+module Io
+	module AST
+    class IOCore
+      def initialize(args)
+        Hash[args]
+      end
+    end
+  end
+end
---
+++ lib/ast/number.rb
@@ -0,0 +1,9 @@
+module Io::AST
+  class Number < IOCore
+    attr_accessor :number
+    
+    def traverse
+    end
+  end
+end
+
