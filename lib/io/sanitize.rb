c9f2cb1d467308e1f775a626dea48ffb32132dcf
Pankaj Doharey <pankajdoharey@gmail.com>
2013-01-21 15:46:30 +0530

    Removed unn-cessary unused methods

--- lib/io/sanitize.rb
+++ lib/io/sanitize.rb
@@ -1,29 +1,7 @@
-module Sanitize
-  #Remove \n line
-  def remove_new_line
-    self.code = code.gsub(/^(\n)+/,"") #Remove New line from beginning
-  end
-  
-  #Getting Rid of more than one space in code 
-  def remove_white_space
-    self.code = code.gsub(/\A\s+/m,"") #Remove Begining Spaces
-    self.code = code.gsub(/\Z\s+/m,"") #Remove Ending   Spaces
-  end
-  
-  #Remove whitespace from beginning and end of line
-  def remove_tail_spaces
-    self.code = code.gsub(/^\s*/,"").gsub(/\s+$/,"")
-  end
-
-  def remove_semicolon
-    ""
-  end
-  
+module Sanitize  
   #Sanitize Input Code
   def sanitize
-    remove_new_line
-    #remove_white_space
-    #remove_tail_spaces
+    #Ignore will use it later to escape strings and unicode characters.
   end
 end
 
