19f2f475540c335b891f34f916bc620bdece89fb
Pankaj Doharey <pankajdoharey@live.com>
2013-01-21 23:48:47 +0530

    Corrected the module loader problem by fixing the path

--- lib/ast.rb
+++ lib/ast.rb
@@ -1,12 +1,12 @@
 module Io
-	module AST
+  module AST
     class IOCore
       def initialize(args)
         Hash[args]
       end
     end
 
-    Dir.glob File.expand_path("./ast/*", __FILE__) do |file|
+    Dir.glob File.expand_path("../ast/*", __FILE__) do |file|
       require file
     end
   end
