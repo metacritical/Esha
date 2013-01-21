0adc6c87954d04c5d507165e8a93841cea057dc0
Pankaj Doharey <pankajdoharey@live.com>
2013-01-21 23:53:15 +0530

    Parser read token corrected

--- src/parser.rb
+++ src/parser.rb
@@ -10,30 +10,12 @@ class Parser
     read_token
     unless look_ahead.nil?
       case look_ahead.type
-      when :SETSLOT    then puts "Setslot"
+      when :SETSLOT    then setslot
       when :IDENTIFIER then setlabel
       end
     end
   end
-
-
-  def read_token
-    @read_token = lexer.read_token
-    puts "Read Token : #{@read_token.inspect}"
-    
-    self.token = read_token
-  end
-
-  def setslot
-    puts "If Reached here #{@token.inspect}"
-    check_type(token)
-    Setslot.new
-  end
-
-  def look_ahead
-    lexer.look_ahead
-  end
-
+  
   private
   def error!(message , tok_obj)
     raise ParseError, message rescue paint("#{message} :: #{tok_obj.line}", :red);exit
@@ -41,6 +23,22 @@ class Parser
   
   def check_type(tok_object)
     error! "Found #{tok_object.type} #{tok_object.value}, unexpected type error", tok_obj unless TOKENS.include? tok_object.type
+  end
+
+  def read_token
+    self.token = self.read_token = lexer.read_token
+  end
+
+  def setslot
+    check_type(token)
+    AST::Setslot.new
+  end
+
+  def setlabel
+  end
+
+  def look_ahead
+    lexer.look_ahead
   end
 
   def statement
