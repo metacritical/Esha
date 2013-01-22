4634d976ac62ff8522e6b70b1da9b6d8aa7158c3
Pankaj Doharey <pankajdoharey@gmail.com>
2013-01-22 14:34:36 +0530

    Updated setslot AST to create set slot objects with left and right attributes

--- lib/ast/setslot.rb
+++ lib/ast/setslot.rb
@@ -2,7 +2,10 @@ module Io::AST
   class Setslot < IOCore
     attr_accessor :slot_name , :slot_value , :left , :right
     
-    def initialize
+    def initialize(options={})
+      options.each do |key,value|
+        send "#{key}=",value
+      end
     end
 
     def traverse
