87f61b39cc5ee185c6b0f3704168fd2f6cf17257
Pankaj Doharey <pankajdoharey@gmail.com>
2013-01-22 19:02:07 +0530

    added Indentifier AST

---
+++ lib/ast/identifier.rb
@@ -0,0 +1,14 @@
+module Io::AST
+  class Identifier < IOCore
+    attr_accessor :iden_name , :iden_value , :left , :right
+    
+    def initialize(options={})
+      options.each do |key,value|
+        send "#{key}=",value
+      end
+    end
+
+    def traverse
+    end
+  end
+end
