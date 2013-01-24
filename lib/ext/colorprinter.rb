ed874c3fe6fd8e51b01ec6697e4bca401e6dad26
Pankaj Doharey <pankajdoharey@gmail.com>
2013-01-24 12:55:13 +0530

    Got rid of Coderay for syntax highlighting in object pretty inspect, cwith code inspect function

--- lib/ext/colorprinter.rb
+++ lib/ext/colorprinter.rb
@@ -14,7 +14,7 @@ module ColorPrinter
   RENDITION_EFFECTS = {
     :bright    => 1,
     :underline => 4,
-    :color256  => 38, #next arguments are ;5;x where x is color index (0..255) {For rgb, 256 term colors}
+    :color256  => 38, #Next arguments are ;5;x where x is color index (0..255) {For rgb, 256 term colors}
   }
 
   ANSI_ESC        = "\e["
@@ -23,10 +23,24 @@ module ColorPrinter
   ANSI_RESET      = "\e[0m"
 
   def paint( input, color )
-    puts "#{ANSI_ESC}#{RENDITION_EFFECTS[:bright]}#{ANSI_FOREGRND}#{ANSI_COLOR_CODES[color]}m#{input}#{ANSI_RESET}"
+   print color_string(input, :red)
   end	
   
   def code_print(message, lang)
     print CodeRay.scan(message, lang).encode(:terminal)
+  end
+
+  def color_string(string ,color)
+    "#{ANSI_ESC}#{RENDITION_EFFECTS[:bright]}#{ANSI_FOREGRND}#{ANSI_COLOR_CODES[color]}m#{string}#{ANSI_RESET}"
+  end
+
+  def code_inspect(object)
+    print object.pretty_inspect
+      .gsub(/\#.+\:\:\w*/){|match| color_string(match,:red) }
+      .gsub(/\@\w*/){|match| color_string(match,:white) }
+      .gsub(/\".+?\"/){|match| color_string(match,:yellow) }
+      .gsub(/\:.+?\w*/){|match| color_string(match,:blue) }
+      .gsub(/\=/){|match| color_string(match,:magenta) }
+      .gsub(/\>/){|match| color_string(match,:red) }
   end
 end
