cd79491514a1a2b0cae7e3b8e58532deefd23727
Pankaj Doharey <pankajdoharey@gmail.com>
2013-01-24 19:18:31 +0530

    Skipped current tokensizer tests  for the time being

--- test/test_tokenizer.rb
+++ test/test_tokenizer.rb
@@ -12,28 +12,31 @@ require 'parser'
 describe Lexer do
   before do
     def lexer
-      @lexer = Lexer.new(test_code)
+      @lexer = Lexer.new(simple_code)
     end
   end
   
   it "should check if the given code is an instance of string" do
+    skip
     self.lexer.code.must_be_instance_of String
   end
   
   it "should tokenize and output the given code" do
-    puts "Input code : #{ code_print(test_code, :ruby) }"
+    #puts "Input code : #{ code_print(test_code, :ruby) }"
     tokens = lexer.tokenize
     puts "Parsed Tokens :\n #{ code_print(tokens.pretty_inspect, :ruby) }"
     puts "Number of Tokens  #{tokens.length}"
   end
 
   it "should read token sequentially using read_token method" do
+    skip
     lexer_instance = lexer
     total_tokens = lexer_instance.tokenize.size
     total_tokens.times{|count| puts "Count : #{count} :\n #{ code_print(lexer_instance.read_token.pretty_inspect, :java) }" }
   end
 
   it "should be able to read the current token and look_ahead one token" do
+    skip
     lexer_instance = lexer
     total_tokens = lexer_instance.tokenize.size
     total_tokens.times do |count|
